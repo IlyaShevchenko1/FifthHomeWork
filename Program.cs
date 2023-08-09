a8ec9e39df8b2de83ae72a7fa1e5b15ca0c32010
Ilya Shevchenko <6BLK@mail.ru>
2023-08-09 22:50:36 +0300

    Second task is done

--- Program.cs
+++ Program.cs
@@ -47,6 +47,53 @@ Console.WriteLine($"Quantity of even numbers in array is {CountOfEvenNumbers(myA
 // Task 36. Задайте одномерный массив, заполненный случайными числами. Найдите
 // сумму элементов, стоящих на нечетных позициях.
 
+/*
+
+
+int[] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();    
+}
+
+int SumOfOddElements(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 1; i < array.Length; i+=2)
+        sum = sum + array[i];
+    
+    return sum;
+}
+
+Console.Write("Input a lenght of array: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value of array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value of array: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(m, a, b);
+Console.Write("Your array is: ");
+WriteArray(myArray);
+
+if (m <= 1) Console.WriteLine("There are no odd elements in the massive");
+else Console.WriteLine($"Sum of odd elements of array is: {SumOfOddElements(myArray)}");
+
+*/
+
 
 // Task 38. Задайте массив вещественных чисел. Найдите разницу между максимальным
 // и минимальным элементом массива.
